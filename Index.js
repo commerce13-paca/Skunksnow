8eea2a463cfa2fdb3ef9419b01729cdf0d6383da
Epicerieoldest <125988115+Epicerieoldest@users.noreply.github.com>
2023-03-24 16:17:11 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,15 +1,33 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
-} else { 
-  console.log("Geolocation is not supported by this browser.");
-}
+const axios = require('axios');
 
-function showPosition(position) {
-  console.log("Latitude: " + position.coords.latitude +
-  "<br>Longitude: " + position.coords.longitude);
-}
+const adAccountId = '20c11c29-6349-4635-9610-e7d77d3ec316';
+const endpoint = `https://adsapi.snapchat.com/v1/adaccounts/${adAccountId}/media`;
+
+axios.get(endpoint, {
+  headers: {
+    'Authorization': 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjU0MDIzMTU2LCJzdWIiOiIyMGMxMWMyOS02MzQ5LTQ2MzUtOTYxMC1lN2Q3N2QzZWMzMTZ-UFJPRFVDVElPTn4zZWRjZWM1Yi0xY2VkLTQ1ZDMtYTQ3OS0yNGVmYmQ2ZGM4OWYifQ.Fn-pmhydMe6tKrlMS6MLYsSs3z0jT1ShnLdmnNE9a8s',
+    'Content-Type': 'application/json'
+  }
+})
+.then(response => {
+  console.log(response.data);
+})
+.catch(error => {
+  console.log(error);
+});
+
+
+
+
+
+
+
+
 
 
 setTimeout(function(){
   document.querySelector('.notification').style.display = 'none';
 }, 2000);
+
+
+
